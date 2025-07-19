2c4922bc71123477a389df328ddf40adc6b73095
yellowstarr0323 <ihansaem60@gmail.com>
2025-07-19 22:55:54 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/domibe/DomiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.domibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomiBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DomiBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/domibe/DomiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomiBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
